0e5aec6964650b0206dd5fa0e27223dcfea33bbc
EliaStein <elias.stein@gmail.com>
2024-11-26 20:08:19 -0500

    Create schools.ts

---
+++ src/components/EssayWizard/constants/schools.ts
@@ -0,0 +1,39 @@
+import type { School } from '../types';
+
+export const SCHOOLS: School[] = [
+  {
+    name: "American University",
+    prompts: [
+      "American University students identify as changemakers and describe themselves as passionate. Describe a belief, hobby, idea, issue, or topic about which you're excited."
+    ]
+  },
+  {
+    name: "Amherst College",
+    prompts: [
+      "What do you see as the benefits of linking learning with leadership and/or service? In your response, please share with us a time where you have seen that benefit through your own experience.",
+      "In what ways could your unique experiences enhance our understanding of our nation and our world?",
+      "Tell us about an intellectual or creative passion you have pursued; what did you learn about yourself through that pursuit?"
+    ]
+  },
+  {
+    name: "Babson College",
+    prompts: [
+      "The Babson education prepares students for all types of careers across business, entrepreneurship, social innovation, and more. Tell us about your interest in this area of study and in Babson specifically.",
+      "A defining element of the Babson experience is learning and thriving in an equitable and inclusive community with a wide range of perspectives and interests. Please share something about your background, lived experiences, or viewpoint(s) that speaks to how you will contribute to and learn from Babson's collaborative community."
+    ]
+  },
+  {
+    name: "Bard College",
+    prompts: [
+      "Why Bard?"
+    ]
+  },
+  {
+    name: "Barnard College",
+    prompts: [
+      "Barnard College is an extraordinary community of women committed to fostering curiosity and the exploration of new experiences and ideas. By utilizing the resources of our campus, our Foundations curriculum, and New York City, our students expand their world and discover their own capabilities. How do you envision these intersecting components of Barnard shaping your academic and personal journey?",
+      "Barnard College students engage in the bold questions that define their generation. Choose one question that you have about the world around you, and explain why it matters to you.",
+      "In college, you will encounter others with diverse viewpoints and experiences. Describe an instance where you engaged with someone who held a different opinion and explain how it shaped your perspective on the issue."
+    ]
+  }
+];
