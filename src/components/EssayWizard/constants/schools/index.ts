8d3c5424ac9985caf2075fcafb507fc1e49e0a53
EliaStein <elias.stein@gmail.com>
2024-11-30 15:50:53 -0500

    removing rogue elements

--- src/components/EssayWizard/constants/schools/index.ts
+++
@@ -1,5 +0,0 @@
-import { SCHOOLS_A_D } from './a-d';
-import { SCHOOLS_E_H } from './e-h';
-import { SCHOOLS_I_Z } from './i-z';
-
-export const SCHOOLS = [...SCHOOLS_A_D, ...SCHOOLS_E_H, ...SCHOOLS_I_Z];
