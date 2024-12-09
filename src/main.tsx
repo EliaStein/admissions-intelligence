8c0896554e84192d3072ea65ded71d550d818111
EliaStein <elias.stein@gmail.com>
2024-12-08 21:11:57 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import emailjs from '@emailjs/browser';
+import { EMAILJS_CONFIG } from './config/emailjs';
 import { App } from './App';
 import './index.css';
+
+// Initialize EmailJS at the application root
+emailjs.init(EMAILJS_CONFIG.PUBLIC_KEY);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
