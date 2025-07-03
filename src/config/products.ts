b259dbf19478d2dc9c0a77548d2730af4206ad67
Koji <stylethewalker@gmail.com>
2025-07-03 18:34:20 -0300

    update production products

--- src/config/products.ts
+++ src/config/products.ts
@@ -48,39 +48,39 @@ const devProducts = [
 ];
 const prodProducts = [
   {
-    id: 'prod_SZ9ZrKVp8D4ChN',
+    id: 'prod_Sc8zf33MR0t3SZ',
     name: 'Single Review',
     credits: 1,
     price: 99,
-    priceId: 'price_1Re1GW02R5keeXtZ7yEPRyIz',
+    priceId: 'price_1Rguh303wFCvtmnbpEXtPYNr',
   },
   {
-    id: 'prod_SZ9fokXOBwQhDd',
+    id: 'prod_Sc8zmJkbM4aT23',
     name: '3 Review Pack',
     credits: 3,
     price: 282,
     originalPrice: 297,
     discount: 5,
-    priceId: 'price_1Re1Ma02R5keeXtZT13yayvt',
+    priceId: 'price_1RguhP03wFCvtmnbTFmCBQHU',
   },
   {
-    id: 'prod_SZ9nIXNbOIMBv2',
+    id: 'prod_Sc90kY3c9LLDL8',
     name: '5 Review Pack',
     credits: 5,
     price: 446,
     originalPrice: 495,
     discount: 10,
-    priceId: 'price_1Re1UG02R5keeXtZEBVyIsRG',
+    priceId: 'price_1Rguih03wFCvtmnbxtFsdBVy',
     popular: true,
   },
   {
-    id: 'prod_SZ9oRH3K1aueQv',
+    id: 'prod_Sc92ZeDwkx5Igc',
     name: '10 Review Pack',
     credits: 10,
     price: 792,
     originalPrice: 990,
     discount: 20,
-    priceId: 'price_1Re1US02R5keeXtZaOHxoeTL',
+    priceId: 'price_1RgukG03wFCvtmnbZYOpgoEc',
   }
 ];
 
