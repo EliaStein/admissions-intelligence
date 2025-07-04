4e3a3dfaa72285d804420607b7a64865304c580b
Koji <stylethewalker@gmail.com>
2025-07-04 08:40:47 -0300

    clean up

--- src/config/emailjs.ts
+++
@@ -1,6 +0,0 @@
-export const EMAILJS_CONFIG = {
-  PUBLIC_KEY: 'Yyt3RCIThYgu1cSg8',
-  SERVICE_ID: 'service_bpavxve',
-  TEMPLATE_ID: 'template_icu1qz2',
-  TO_EMAIL: 'info@admissionsintelligence.ai'
-};
